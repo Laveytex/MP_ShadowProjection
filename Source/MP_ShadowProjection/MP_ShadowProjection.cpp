9df8dead6d69c64065c8a4a6f60869c4f79d23e7
Laveytex <YanAlex13@mail.ru>
2024-12-23 15:46:52 +0300

    Initial commit

---
+++ Source/MP_ShadowProjection/MP_ShadowProjection.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MP_ShadowProjection.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MP_ShadowProjection, "MP_ShadowProjection" );
